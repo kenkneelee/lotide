12bc1b71902e911205d521e4e0d1027736a646d3
Kenny Lee <kenny.lee927@gmail.com>
2023-07-12 17:19:11 -0400

    create assertEqual function to compare two inputs and log verbose pass/fail messages

---
+++ assertEqual.js
@@ -0,0 +1,15 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  actual === expected
+    ? console.log("🚀🚀🚀Assertion Passed: " + actual + " === " + expected)
+    : console.log("❗️❗️❗️Assertion Failed: " + actual + " !== " + expected);
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("1", 1);
+assertEqual("42", "Fourty two");
+assertEqual("true", true);
+assertEqual("Lighthouse", "lighthouse");
+assertEqual("Lighthouse", "Lighthouse");
