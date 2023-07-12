45a4ec6efc4c3e1a7d659b94a5ae9c86d8a00de3
Kenny Lee <kenny.lee927@gmail.com>
2023-07-12 17:34:54 -0400

    update assertEquals to use template literals instead of concat

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 // FUNCTION IMPLEMENTATION
 const assertEqual = function(actual, expected) {
   actual === expected
-    ? console.log("🚀🚀🚀Assertion Passed: " + actual + " === " + expected)
-    : console.log("❗️❗️❗️Assertion Failed: " + actual + " !== " + expected);
+    ? console.log(`🚀🚀🚀Assertion Passed: ${actual} === ${expected}`)
+    : console.log(`❗️❗️❗️Assertion Failed: ${actual} !== ${expected}`);
 };
 
 // TEST CODE
