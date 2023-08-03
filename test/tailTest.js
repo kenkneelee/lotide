03f7f31d2aeba7f85a8fa441a155c9fac6741234
Kenny Lee <kenny.lee927@gmail.com>
2023-08-03 19:54:59 -0400

    convert from simple assertion testing to mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,23 +1,30 @@
-const assertEqual = require(`../assertEqual`);
-const assertArraysEqual = require(`../assertArraysEqual`);
-const tail = require (`../tail`)
+const assert = require("chai").assert;
+const tail = require(`../tail`);
 
 // TEST CODE
-// Test Case 1: Array of numbers
-assertArraysEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
-// Test Case 2: Array of strings
-assertArraysEqual(tail(["a", "b", "c", "d"]), ["b", "c", "d"]);
-// Test Case 3: Array of mixed strings and numbers
-assertArraysEqual(tail([5, "a", 4, "b", 3]), ["a", 4, "b", 3]);
-// Test Case 4: Array with a single element
-assertArraysEqual(tail([1]), []);
-// Test Case 5: Empty array
-assertArraysEqual(tail([]), []);
-// Test Case 6: Not an array
-assertEqual(tail("NotArray"), undefined);
-
-
-// // Test Case: Check the original array
-// const words = ["Yo Yo", "Lighthouse", "Labs"];
-// tail(words); // no need to capture the return value since we are not checking it
-// assertEqual(words.length, 3); // original array should still have 3 elements!
+describe("#tail", () => {
+  // Test Case 1: Array of numbers
+  it("returns [2, 3, 4, 5] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(tail([1, 2, 3, 4, 5]), [2, 3, 4, 5]);
+  });
+  // Test Case 2: Array of strings
+  it("returns ['b', 'c', 'd'] for ['a', 'b', 'c', 'd']", () => {
+    assert.deepEqual(tail(["a", "b", "c", "d"]), ["b", "c", "d"]);
+  });
+  // Test Case 3: Array of mixed strings and numbers
+  it("returns ['a', 4, 'b', 3] for [5, 'a', 4, 'b', 3]", () => {
+    assert.deepEqual(tail([5, "a", 4, "b", 3]), ["a", 4, "b", 3]);
+  });
+  // Test Case 4: Array with a single element
+  it("returns [] for [1]", () => {
+    assert.deepEqual(tail([1]), []);
+  });
+  // Test Case 5: Empty array
+  it("returns [] for []", () => {
+    assert.deepEqual(tail([]), []);
+  });
+  // // Test Case 6: Not an array
+  it("returns undefined for non array input", () => {
+    assert.strictEqual(tail("NotArray"), undefined);
+  });
+});
