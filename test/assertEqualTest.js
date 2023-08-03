52c9a54b69b810e3a13bdb2a7dcaabc3383fbb25
Kenny Lee <kenny.lee927@gmail.com>
2023-08-03 17:52:22 -0400

    add comments to test cases, add test cases

--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,10 +1,21 @@
 const assertEqual = require (`../assertEqual`)
 
 // TEST CODE
-assertEqual("Lighthouse Labs", "Bootcamp");
+// should pass, strict equality of numbers
 assertEqual(1, 1);
-assertEqual("1", 1);
-assertEqual("42", "Fourty two");
-assertEqual("true", true);
-assertEqual("Lighthouse", "lighthouse");
+// should pass, strict equality of strings
 assertEqual("Lighthouse", "Lighthouse");
+// should pass, strict equality of booleans
+assertEqual(true, true);
+// should fail, inequality of strings
+assertEqual("Lighthouse Labs", "Bootcamp");
+// should fail, different data types so not strict equality
+assertEqual("1", 1);
+// should fail, inequality of strings
+assertEqual("42", "Fourty two");
+// should fail, different data types string - boolean
+assertEqual("true", true);
+// should fail, different casing
+assertEqual("Lighthouse", "lighthouse");
+// should fail, not strict equality
+assertEqual(true, 1);
