2ef1f0047eca585f03e746c82cab9c5b91611d63
Kenny Lee <kenny.lee927@gmail.com>
2023-08-03 19:43:00 -0400

    change to using mocha and chai testing suite from previous simple assertions

--- test/headTest.js
+++ test/headTest.js
@@ -1,19 +1,41 @@
-const assertEqual = require (`../assertEqual`);
-const head = require(`../head`)
+const assert = require("chai").assert;
+const head = require("../head");
 
-// TEST CODE
-// should all pass
-// array of numbers
-assertEqual(head([5, 6, 7]), 5);
-// array of strings
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-// array of single number
-assertEqual(head([1]), 1);
-// empty array
-assertEqual(head([]), undefined);
-// no input
-assertEqual(head(), undefined);
-// array of strings
-assertEqual(head(["boot", "camp"]), "boot");
-// array of mixed strings and numbers
-assertEqual(head(["1", 2, 3]), "1");
+describe("#head", () => {
+  // array of numbers
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+  // array of numbers
+  it("returns 5 for [5, 6, 7]", () => {
+    assert.strictEqual(head([5, 6, 7]), 5);
+  });
+  // array of single string
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(["5"]), "5");
+  });
+  // array of single number
+  it("returns 1 for [1]", () => {
+    assert.strictEqual(head([1]), 1);
+  });
+  // array of strings
+  it("returns 'Hello' for ['Hello', 'Lighthouse', 'Labs']", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+  // array of strings
+  it("returns 'boot' for ['boot', 'camp']", () => {
+    assert.strictEqual(head(["boot", "camp"]), "boot");
+  });
+  // array of mixed strings and numbers
+  it("returns '1' for ['1', 2, 3]", () => {
+    assert.strictEqual(head(["1", 2, 3]), "1");
+  });
+  // empty array
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+  // no input
+  it("returns undefined for no input", () => {
+    assert.strictEqual(head(), undefined);
+  });
+});
