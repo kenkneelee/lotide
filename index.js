92b9ae578ba05d21429a7dfaa252a4ab7f8cb6f7
Kenny Lee <kenny.lee927@gmail.com>
2023-08-03 23:17:41 -0400

    Add all files to index.js export

--- index.js
+++ index.js
@@ -1,16 +1,52 @@
 // index.js
-const head = require("./head");
-const tail = require("./tail");
-const middle = require("./middle");
+// const head = require("./head");
+// const tail = require("./tail");
+// const middle = require("./middle");
+// const assertEqual = require("./assertEqual");
+// const eqArrays = require("./eqArrays");
+// const assertArraysEqual = require("./assertArraysEqual");
+
+// Equality and Assertion Functions
 const assertEqual = require("./assertEqual");
 const eqArrays = require("./eqArrays");
 const assertArraysEqual = require("./assertArraysEqual");
+const eqObjects = require("./eqObjects");
+const assertObjectsEqual = require("./assertObjectsEqual");
+
+// Array Functions
+const head = require("./head");
+const tail = require("./tail");
+const middle = require("./middle");
+const flatten = require("./flatten");
+const without = require("./without");
+
+// Array and Object Manipulation
+const countLetters = require("./countLetters");
+const countOnly = require("./countOnly");
+const letterPositions = require("./letterPositions");
+
+// Functions using Callbacks
+const findKey = require("./findKey");
+const findKeyByValue = require("./findKeyByValue");
+const map = require("./map");
+const takeUntil = require("./takeUntil");
 
 module.exports = {
-  head,
-  tail,
-  middle,
   assertEqual,
   eqArrays,
   assertArraysEqual,
+  eqObjects,
+  assertObjectsEqual,
+  head,
+  tail,
+  middle,
+  flatten,
+  without,
+  countLetters,
+  countOnly,
+  letterPositions,
+  findKey,
+  findKeyByValue,
+  map,
+  takeUntil,
 };
