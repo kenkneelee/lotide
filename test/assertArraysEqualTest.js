e2d8c65d9453691067fdefacfb75fd77faf258c7
Kenny Lee <kenny.lee927@gmail.com>
2023-08-04 01:20:39 -0400

    Add to comments to clarify test cases

--- test/assertArraysEqualTest.js
+++ test/assertArraysEqualTest.js
@@ -1,13 +1,14 @@
+// Import function to test
 const assertArraysEqual = require(`../assertArraysEqual`);
 // TEST CODE
-// Equal arrays of numbers
+// Test case 1:Equal arrays of numbers
 assertArraysEqual([1, 2, 3], [1, 2, 3]); // Should PASS
-// Empty arrays
+// Test case 2:Empty arrays
 assertArraysEqual([], []); // Should PASS
-// Arrays with one element
+// Test case 3:Arrays with one element
 assertArraysEqual([1], [1]); // Should PASS
 
-// Arrays with same values but different types
+// Test case 4:Arrays with same values but different types
 assertArraysEqual([1, "2", 3], [1, 2, 3]); // Should FAIL
-// Arrays with same values, one all strings, other all numbers
+// Test case 5:Arrays with same values, one all strings, other all numbers
 assertArraysEqual(["1", "2", "3"], [1, 2, 3]); // Should FAIL
--- test/assertEqualTest.js
+++ test/assertEqualTest.js
@@ -1,21 +1,21 @@
-const assertEqual = require (`../assertEqual`)
-
+// Import function to test
+const assertEqual = require(`../assertEqual`);
 // TEST CODE
-// should pass, strict equality of numbers
+// Test case 1:should pass, strict equality of numbers
 assertEqual(1, 1);
-// should pass, strict equality of strings
+// Test case 2:should pass, strict equality of strings
 assertEqual("Lighthouse", "Lighthouse");
-// should pass, strict equality of booleans
+// Test case 3:should pass, strict equality of booleans
 assertEqual(true, true);
-// should fail, inequality of strings
+// Test case 4:should fail, inequality of strings
 assertEqual("Lighthouse Labs", "Bootcamp");
-// should fail, different data types so not strict equality
+// Test case 5:should fail, different data types so not strict equality
 assertEqual("1", 1);
-// should fail, inequality of strings
+// Test case 6:should fail, inequality of strings
 assertEqual("42", "Fourty two");
-// should fail, different data types string - boolean
+// Test case 7:should fail, different data types string - boolean
 assertEqual("true", true);
-// should fail, different casing
+// Test case 8:should fail, different casing
 assertEqual("Lighthouse", "lighthouse");
-// should fail, not strict equality
+// Test case 9:should fail, not strict equality
 assertEqual(true, 1);
