eba80a76ede41a736a81f69bd7cfa73cb51c7882
Kenny Lee <kenny.lee927@gmail.com>
2023-08-04 04:01:13 -0400

    refactor, modularize, separate test code, convert to mocha chai testing

---
+++ test/findKeyTest.js
@@ -0,0 +1,59 @@
+// Import function to test
+const findKey = require("../findKey");
+// HELPER FUNCTIONS
+const assert = require("chai").assert;
+
+// TEST CODE
+describe("#findKey", () => {
+  // Test Case: Given in assignment, multiple matching keys
+  it("returns 'noma' in object", () => {
+    assert.strictEqual(
+      findKey(
+        {
+          "Blue Hill": { stars: 1 },
+          "Akaleri": { stars: 3 },
+          "noma": { stars: 2 },
+          "elBulli": { stars: 3 },
+          "Ora": { stars: 2 },
+          "Akelarre": { stars: 3 },
+        },
+        (x) => x.stars === 2
+      ),
+      "noma"
+    );
+  });
+  // Test Case: No matching keys
+  it("returns undefined", () => {
+    assert.strictEqual(
+      findKey({ a: 1, b: 2, c: 3, d: 4 }, (x) => x === 5),
+      undefined
+    );
+  });
+  // Test Case: Object with Number key values
+  it("returns 'f' in object", () => {
+    assert.strictEqual(
+      findKey({ a: 3, b: 1, c: 4, d: 1, e: 5, f: 9 }, (x) => x > 8),
+      "f"
+    );
+  });
+  // Test Case: Object with String key values
+  it("returns 'Toyota' in object", () => {
+    assert.strictEqual(
+      findKey(
+        { Honda: "Civic", Toyota: "Corolla", Mazda: "3", Nissan: "Sentre" },
+        (x) => x === "Corolla"
+      ),
+      "Toyota"
+    );
+  });
+  // Test Case: Object with Boolean key values
+  it("returns 'd' in object", () => {
+    assert.strictEqual(
+      findKey(
+        { a: true, b: true, c: true, d: false, e: true, f: false },
+        (x) => !x
+      ),
+      "d"
+    );
+  });
+});
