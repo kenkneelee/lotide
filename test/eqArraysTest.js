44cb2d4ddd90992c686bc07806342959e4d0bac5
Kenny Lee <kenny.lee927@gmail.com>
2023-08-04 01:46:06 -0400

    separate test code and make it work with mocha/chai

--- test/eqArraysTest.js
+++ test/eqArraysTest.js
@@ -1,14 +1,33 @@
-const assertEqual = require(`../assertEqual`);
+// Import function to test
 const eqArrays = require(`../eqArrays`);
+// Import test helper function
+const assert = require("chai").assert;
 
 // TEST CODE
-// Equal arrays of numbers
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-// Arrays with same values but different types
-assertEqual(eqArrays([1, "2", 3], [1, 2, 3]), false); // => should PASS
-// Arrays with same values, one all strings, other all numbers
-assertEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false); // => should PASS
-// Empty arrays
-assertEqual(eqArrays([], []), true); // => should PASS
-// Arrays with one element
-assertEqual(eqArrays([1], [1]), true); // => should PASS
+describe("#eqArrays", () => {
+  // Equal arrays of numbers
+  it("returns true for [1, 2, 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+  });
+  // Arrays with same values but different types
+  it("returns false for [1, '2', 3], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays([1, "2", 3], [1, 2, 3]), false);
+  });
+  // Arrays with same values, one all strings, other all numbers
+  it("returns false for ['1', '2', '3'], [1, 2, 3]", () => {
+    assert.strictEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
+  });
+  // Empty arrays
+  it("returns true for [], []", () => {
+    assert.strictEqual(eqArrays([], []), true);
+  });
+  // Arrays with one element
+  it("returns true for [1], [1]", () => {
+    assert.strictEqual(eqArrays([1], [1]), true);
+  });
+});
+assert.strictEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+assert.strictEqual(eqArrays([1, "2", 3], [1, 2, 3]), false);
+assert.strictEqual(eqArrays(["1", "2", "3"], [1, 2, 3]), false);
+assert.strictEqual(eqArrays([], []), true);
+assert.strictEqual(eqArrays([1], [1]), true);
